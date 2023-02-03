278b033dc88213f1459b621e4f3d639b7633e37f
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2023-02-03 14:24:35 +0300

    create rating html and its view function

--- gameapp/views.py
+++ gameapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from gameapp.forms import GameForm
+from django.shortcuts import render, redirect, get_object_or_404
+from gameapp.forms import GameForm, RateForm
 from gameapp.models import Game, Profile, Rate
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+
 
 
 
@@ -33,3 +35,28 @@ def game_view(request,id):
     game = Game.objects.get(id = id)
     rates = Rate.objects.all()
     return render(request, 'game_view.html',{"rates":rates,"game":game})
+
+@login_required(login_url='/accounts/login/')
+def rate_game(request,game_id):
+    games = Game.game_by_id(id=game_id)
+    game = get_object_or_404(Game, pk=game_id)
+    current_user = request.user
+    if request.method == 'POST':
+        form = RateForm(request.POST)
+        if form.is_valid():
+
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            rate = Rate()
+            rate.game = game
+            rate.user = current_user
+            rate.design = design
+            rate.usability = usability
+            rate.content = content
+            rate.average = (rate.design + rate.usability + rate.content)/3
+            rate.save()
+            return HttpResponseRedirect(reverse('ratedetails', args=(rate.id,)))
+    else:
+        form = RateForm()
+    return render(request, 'rates.html', {"user":current_user,"game":games,"form":form})
